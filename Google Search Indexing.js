b99a28c3a0b9412e26b32d38d9158ab9840ef44a
GowthamThangamani <vtgowtham1993@gmail.com>
2019-11-09 11:18:04 +0530

    Create Google Search Indexing.js

---
+++ Google Search Indexing.js
@@ -0,0 +1,3 @@
+javascript:function E(){var x=document.getElementsByClassName("bkWMgd");var rc=document.getElementsByClassName("rc");for(var i=0;i<rc.length;i++){var textnode=document.createTextNode("GTNO:"+(i+1));console.log(rc[i].appendChild(textnode))}
+console.log(rc);console.log(x);alert("Thank you for using GT Script.")}
+E()
