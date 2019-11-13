fa779bba0114ef5b18855935f6ebf4301a187d22
GowthamThangamani <vtgowtham1993@gmail.com>
2019-11-13 11:15:16 +0530

    Update Google Search Indexing.js

--- Google Search Indexing.js
+++ Google Search Indexing.js
@@ -1,3 +1,5 @@
-javascript:function E(){var x=document.getElementsByClassName("bkWMgd");var rc=document.getElementsByClassName("rc");for(var i=0;i<rc.length;i++){var textnode=document.createTextNode("GTNO:"+(i+1));console.log(rc[i].appendChild(textnode))}
+javascript:function E(){var rem=document.getElementsByClassName("xpdopen");for(var i=0;i<rem.length;i++){rem[i].innerHTML=""}
+var urls=document.getElementsByClassName("iUh30");alert(urls.length);var alertString="";for(var i=0;i<urls.length;i++){var n=urls[i].innerText.includes("rajasri.com");if(n){alertString+="GTNO:"+(i+1)+" URL : "+urls[i].innerText+"\n"}}
+alert(alertString);var x=document.getElementsByClassName("bkWMgd");var rc=document.getElementsByClassName("rc");for(var i=0;i<rc.length;i++){var textnode=document.createTextNode("GTNO:"+(i+1));console.log(rc[i].appendChild(textnode))}
 console.log(rc);console.log(x);alert("Thank you for using GT Script.")}
 E()
